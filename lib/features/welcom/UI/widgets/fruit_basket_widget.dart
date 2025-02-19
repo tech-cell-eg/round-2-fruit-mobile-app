725837e7efe5f338d9bf0b550fc9e36ccddc28c4
Nourhan Bakry <152170075+nourhanbakry@users.noreply.github.com>
2025-02-19 12:33:23 +0200

    feature/welcome screen
    - Added welcome screen

---
+++ lib/features/welcom/UI/widgets/fruit_basket_widget.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:fruit_app/core/constants/app_colors.dart';
+import 'package:fruit_app/core/constants/app_images.dart';
+import 'package:fruit_app/core/helper/extensions.dart';
+
+class FruitBasketWidget extends StatelessWidget {
+  const FruitBasketWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    double contianerHeight = context.screenHeight() * 0.60;
+    return Container(
+      height: contianerHeight,
+      width: context.screenWidth(),
+      color: AppColors.primaryColor,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Padding(
+            padding: EdgeInsets.only(left: context.screenWidth() * 0.7),
+            child: Image.asset(
+              AppImages.imagesFruitDrop1,
+              width: 50,
+              height: 37,
+            ),
+          ),
+          Image.asset(AppImages.imagesFruitsBasket1),
+          SizedBox(height: 8),
+          Image.asset(
+            AppImages.imagesShadow1,
+            width: 301,
+            height: 12,
+          ),
+          SizedBox(height: 34),
+        ],
+      ),
+    );
+  }
+}
